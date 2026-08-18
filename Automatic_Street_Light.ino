b2ee6db3257a80c0f948df8fb757e73e89a0ce47
RakshithaJowler <rakshithajn7@gmail.com>
2026-08-18 21:34:42 +0530

    Create Automatic_Street_Light.ino

---
+++ Automatic_Street_Light.ino
@@ -0,0 +1,17 @@
+int led = 13;
+int button = 2;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(button);
+
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
